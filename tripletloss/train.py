1bd59a1fdfdd894d7087c453d7c1fdf09c91526e
JoeFannie <865365468@qq.com>
2017-07-24 16:35:00 +0800

    Update train.py

--- tripletloss/train.py
+++ tripletloss/train.py
@@ -3,6 +3,25 @@
 # Copyright (c) 2015 Pinguo Tech.
 # Written by David Lu
 # --------------------------------------------------------
+"""Set up paths for tripletloss"""
+
+import os.path as osp
+import sys
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+this_dir = osp.dirname(__file__)
+
+# Add caffe to PYTHONPATH
+caffe_path = osp.join('/gruntdata/DL_dataset/caojiajiong/caffe-windows/', 'python')
+add_path(caffe_path)
+
+# Add lib to PYTHONPATH
+lib_path = osp.join(this_dir)
+add_path(lib_path)
+
 
 """Train the network."""
 
